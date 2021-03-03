8ab09b0621908d9305895e7ab53d229cca5aae6d
maulonobile <50904371+maulonobile@users.noreply.github.com>
2021-03-03 10:37:40 +0100

    Update mpv_youtube.sh
    aggiunta descrizione

--- mpv_youtube.sh
+++ mpv_youtube.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#########################################################################
+# Guardare Youtube in SD con mpv da riga di comando e risparmiare risorse
+# URL di esempio:
 # https://www.youtube.com/watch?v=bo9Z_pgByQY
 # https://youtu.be/bo9Z_pgByQY
+#########################################################################
 echo $1
 url=$(echo $1|sed 's/^.*=/https:\/\/youtu.be\//')
 echo $url
-
-#mpv --ytdl-format=135+139 $url || mpv --ytdl-format=244+249 $url || mpv --ytdl-format=135+249 $url || mpv --ytdl-format=94 $url
 mpv --ytdl-format=244+249 $url || mpv --ytdl-format=244+139 $url || mpv --ytdl-format=135+139 $url || mpv --ytdl-format=135+249 $url || mpv --ytdl-format=94 $url || mpv --ytdl-format=18+249 $url
