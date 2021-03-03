b7952a2b17ac1aefe0d249863b303ff2837ff64f
maulonobile <50904371+maulonobile@users.noreply.github.com>
2021-03-03 10:39:14 +0100

    Update onedance.sh
    aggiustamenti descrizione

--- onedance.sh
+++ onedance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#################################################################
-# Ascoltare OndeDance.fm da riga di comando con mpv e stream AAC#
-#################################################################
+#####################################################################
+# Ascoltare OndeDance.fm da riga di comando con mpv e stream AAC 64k
+#####################################################################
 echo 'One Dance stream AAC'
 mpv https://ice04.fluidstream.net/rn1_2.aac?FLID=1
