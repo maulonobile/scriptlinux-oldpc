135fcbf3e44c4e0aafb1836c882a940bb36b1fe9
maulonobile <50904371+maulonobile@users.noreply.github.com>
2021-03-03 10:38:29 +0100

    Update mpv_youtube_hd.sh
    aggiunta descrizione

--- mpv_youtube_hd.sh
+++ mpv_youtube_hd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+#########################################################################
+# Guardare Youtube in HD con mpv da riga di comando e risparmiare risorse
+# URL di esempio:
 # https://www.youtube.com/watch?v=bo9Z_pgByQY
 # https://youtu.be/bo9Z_pgByQY
+#########################################################################
 echo $1
 url=$(echo $1|sed 's/^.*=/https:\/\/youtu.be\//')
 echo $url
-
-#mpv --ytdl-format=135+139 $url || mpv --ytdl-format=244+249 $url || mpv --ytdl-format=135+249 $url || mpv --ytdl-format=94 $url
 mpv --ytdl-format=247+249 $url 
