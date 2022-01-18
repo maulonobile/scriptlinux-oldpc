aeb12c36a81a1699880ad13154b8ce267b4fa3fb
mauri <mauri@localhost.localdomain>
2022-01-18 10:20:36 +0100

    aggiornato indirizzo streaming

--- onedance.sh
+++ onedance.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #####################################################################
-# Ascoltare OndeDance.fm da riga di comando con mpv e stream AAC 64k
+# Ascoltare OneDanceCH.fm da riga di comando con mpv e stream AAC 
 #####################################################################
-echo 'One Dance stream AAC'
-mpv https://ice04.fluidstream.net/rn1_2.aac?FLID=1
+echo 'One Dance CH stream AAC'
+mpv https://ice15.fluidstream.net/onedancech.aac?FLID=1
+
+
+#one dance italia mpv https://ice04.fluidstream.net/rn1_2.aac?FLID=1
