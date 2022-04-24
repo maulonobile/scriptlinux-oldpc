14ec5b24e01e3920ec4a36c566638047c09d6dfd
maulonobile <maurizio.lonobile@gmail.com>
2022-04-24 13:50:26 +0200

    aggiunto discoradio.sh e aggiornato readme

---
+++ discoradio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#####################################################################
+# Ascoltare Discoradio.it da riga di comando con mpv e stream AAC 
+####################################################################
+echo 'Discoradio stream AAC'
+mpv https://stream.discoradio.radio/audio/disco.stream_aac64/playlist.m3u8
+
